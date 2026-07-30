0761fa16e1f0c283ad5914278e7dda272d4384e9
SyedKhadeer-51 <syed4623khadeer@gmail.com>
2026-07-30 20:10:43 +0530

    chore : sprint3 : Foundation configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,22 @@
-
-
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        
+    <div className="flex flex-1 flex-col items-center justify-center bg-zinc-50 dark:bg-black">
+      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between bg-white px-16 py-32 dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          <h1 className="max-w-xs font-serif text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             This is the start of FixFit platform
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Place your orders now at 
+            Place your orders now at{" "}
             <a
               href="https://fixfit.in/"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
-            {" "}    FixFit.in
+              FixFit.in
             </a>{" "}
-            or the
-            center.
+            or the center.
           </p>
         </div>
-       
       </main>
     </div>
   );
